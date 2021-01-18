0e068a4724ff0ab0d50a94e7376765e552649b34
John Connerton <conjohnerton@gmail.com>
2021-01-17 21:14:14 -0500

    Changed Client App to useState and added button to fire set value function.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 
@@ -14,48 +14,61 @@ const App = () => {
     contract: null,
   });
 
-  useEffect(() => {
-    const connectToNetwork = async () => {
-      try {
-        // Get network provider and web3 instance.
-        const web3 = await getWeb3();
+  const connectToNetwork = useCallback(async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
 
-        // Use web3 to get the user's accounts.
-        const accounts = await web3.eth.getAccounts();
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
 
-        // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = SimpleStorageContract.networks[networkId];
-        const instance = new web3.eth.Contract(
-          SimpleStorageContract.abi,
-          deployedNetwork && deployedNetwork.address
-        );
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
 
-        // Set web3, accounts, and contract to the state, and then proceed with an
-        // example of interacting with the contract's methods.
-        setNetworkState({ web3, accounts, contract: instance });
-        runExample();
-      } catch (error) {
-        // Catch any errors for any of the above operations.
-        alert(
-          `Failed to load web3, accounts, or contract. Check console for details.`
-        );
-        console.error(error);
-      }
-    };
-
-    connectToNetwork();
-  }, [networkState]);
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      const state = {
+        ...networkState,
+        web3,
+        accounts,
+        contract: instance,
+      };
+      setNetworkState(state);
+      return state;
+    } catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
+      return networkState;
+    }
+  });
 
   const runExample = async () => {
-    // const { accounts, contract } = this.state;
+    let network = networkState;
+    if (!networkState.contract || !networkState.accounts) {
+      network = await connectToNetwork();
+    }
+
+    const { accounts, contract } = network;
+
     // Stores a given value, 5 by default.
-    // await contract.methods.set(1).send({ from: accounts[0] });
-    // // Get the value from the contract to prove it worked.
-    // const response = await contract.methods.get().call();
-    // // Update state with the result.
-    // this.setState({ storageValue: response });
+    await contract.methods.set(1).send({ from: accounts[0] });
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
+    // Update state with the result.
+    setNetworkState({ ...networkState, storageValue: response });
   };
+
+  useEffect(() => {
+    connectToNetwork();
+    // runExample();
+  }, []);
 
   if (!networkState.web3) {
     return <div>Loading Web3, accounts, and contract...</div>;
@@ -74,6 +87,12 @@ const App = () => {
       <p>
         Try changing the value stored on <strong>line 40</strong> of App.js.
       </p>
+      <button
+        onClick={runExample}
+        className="bg-red-500 pl-5 pr-5 pt-1 pb-1 rounded text-white"
+      >
+        Click to change value
+      </button>
       <div>The stored value is: {networkState.storageValue}</div>
     </div>
   );
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +9,15 @@ module.exports = {
   networks: {
     develop: {
       port: 7545,
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          process.env.KOVAN_ENDPOINT
+        );
+      },
+      network_id: 42,
     },
   },
 };
