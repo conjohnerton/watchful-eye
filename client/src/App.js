14b682e43439c1c6e1ec1fedad6b8b0279875dde
John Connerton <conjohnerton@gmail.com>
2021-01-19 15:31:16 -0500

    Added AAVE contracts and compiled to JSON artifacts for client.
    Notes:
     - Added a meter that shows the health factor of your AAVE positions.
     - Hard-coded AAVE testnet addresses into client.
     - runExample now gets the health factor of the current user.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useState } from "react";
+import web3 from "web3";
+import LendingPoolContract from "./contracts/LendingPool.json";
+import LendingPoolAddressesProviderContract from "./contracts/LendingPoolAddressesProvider.json";
 import getWeb3 from "./getWeb3";
 
 import Nav from "./components/Nav";
+import Meter from "./components/Meter";
 
 import "./App.css";
 
@@ -14,6 +17,8 @@ const App = () => {
     contract: null,
   });
 
+  const [healthFactor, setHealthFactor] = useState(-1);
+
   const connectToNetwork = async () => {
     try {
       // Get network provider and web3 instance.
@@ -23,11 +28,24 @@ const App = () => {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address
+      // const networkId = await web3.eth.net.getId();
+      // const deployedNetwork = LendingPoolContract.networks[networkId];
+
+      // 0x9C6C63aA0cD4557d7aE6D9306C06C093A2e35408 on aave flashloan repo
+      // 0x88757f2f99175387ab4c6a4b3067c77a695b0349 on aave contracts page
+      const LendingPoolAddressesProviderInstance = new web3.eth.Contract(
+        LendingPoolAddressesProviderContract.abi,
+        "0x88757f2f99175387ab4c6a4b3067c77a695b0349"
+      );
+      const lendingPool = await LendingPoolAddressesProviderInstance.methods
+        .getLendingPool()
+        .call();
+
+      console.log(lendingPool);
+
+      const LendingPoolInstance = new web3.eth.Contract(
+        LendingPoolContract.abi,
+        lendingPool
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -36,7 +54,7 @@ const App = () => {
         ...networkState,
         web3,
         accounts,
-        contract: instance,
+        contract: LendingPoolInstance,
       };
       setNetworkState(state);
       return state;
@@ -56,13 +74,22 @@ const App = () => {
     }
 
     const { accounts, contract } = network;
+    // await contract.methods.
 
+    const userData = await contract.methods
+      .getUserAccountData(accounts[0])
+      .call();
+    setHealthFactor(
+      parseFloat(web3.utils.fromWei(userData.healthFactor)).toFixed(3)
+    );
+    console.log(await contract.methods.getUserAccountData(accounts[0]).call());
+    console.log(
+      await contract.methods
+        .getReserveData("0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD")
+        .call()
+    );
     // Stores a given value, 5 by default.
-    await contract.methods.set(1).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-    setNetworkState({ ...networkState, storageValue: response });
+    // await contract.methods.set(1).send({ from: accounts[0] });
   };
 
   if (!networkState.web3) {
@@ -74,6 +101,18 @@ const App = () => {
     );
   }
 
+  const healthFactorMeter =
+    healthFactor > 10 ? (
+      <p>
+        Woah there, you've got a <strong>huge</strong> health factor. Do you
+        have a loan with AAVE yet?
+      </p>
+    ) : (
+      healthFactor > -1 && (
+        <Meter max={5} min={0} value={healthFactor} unit={"HF"} />
+      )
+    );
+
   return (
     <div className="App">
       <Nav
@@ -81,23 +120,26 @@ const App = () => {
         walletAddress={networkState.accounts[0]}
         connectToNetwork={connectToNetwork}
       />
-      <h1>Good to Go!</h1>
-      <p>Your Truffle Box is installed and ready.</p>
-      <h2>Smart Contract Example</h2>
-      <p>
-        If your contracts compiled and migrated successfully, below will show a
-        stored value of 5 (by default).
-      </p>
-      <p>
-        Try changing the value stored on <strong>line 40</strong> of App.js.
-      </p>
-      <button
-        onClick={runExample}
-        className="bg-red-500 pl-5 pr-5 pt-1 pb-1 rounded text-white"
-      >
-        Click to change value
-      </button>
-      <div>The stored value is: {networkState.storageValue}</div>
+      <div className="flex flex-col items-center space-y-3">
+        <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 40</strong> of App.js.
+        </p>
+        {healthFactorMeter}
+        <button
+          onClick={runExample}
+          className="bg-blue-500 pl-5 pr-5 pt-1 pb-1 rounded text-white"
+        >
+          Click to get AAVE health factor!
+        </button>
+        <div>Your health factor is: {healthFactor}</div>
+      </div>
     </div>
   );
 };
