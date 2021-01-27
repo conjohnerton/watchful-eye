345df147928cd19b56b5ade33b327327fdd16077
John Connerton <conjohnerton@gmail.com>
2021-01-27 11:25:41 -0500

    Added watchful eye logo to things.
    Notes:
     - Dang thing still shows the scaffold-eth logo, even though I deleted it!

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { PageHeader } from "antd";
+import { Image, PageHeader } from "antd";
+// import { WatchfulEye } from "../index";
 
 export default function Header() {
   return (
     <a href="https://github.com/conjohnerton/watchful-eye" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🫖 Watchful Eye"
+        title="👁️ Watchful Eye"
         subTitle="keeps a watchful eye on your loans... uses Aave flashloans to self-liquidate in case things go downhill ⛷️"
         style={{ cursor: "pointer" }}
+        backIcon={<Image src="%PUBLIC_URL%/WatchfulEye.png" />}
       />
     </a>
   );
